0795247d539cc752f8635b6aaa541e4f3a23eec7
Vivek Sharma <viveksharma5970@gmail.com>
2025-08-06 18:22:31 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,20 +5,33 @@ import Home from "./pages/Home";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Zoom } from "react-toastify";
 export default function App() {
 
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/home" element={ <ProtectedRoute> <Home /> </ProtectedRoute> } />
-      </Routes>
-    </Router>
-    <ToastContainer position="top-center" autoClose={3000} />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<ProtectedRoute> <Home /> </ProtectedRoute>} />
+        </Routes>
+      </Router>
+      <ToastContainer
+        position="top-right"
+        autoClose={1000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable={false}
+        pauseOnHover={false}
+        theme="light"
+        transition={Zoom}
+      />
     </>
-    
+
   );
 }
